8ae2568edcee7ed25f112380fd37f461d451ab58
hp <maheshsraut23@gmail.com>
2023-06-14 11:56:09 +0530

    added add method with new calculator class

--- First/First/Program.cs
+++ First/First/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Thank you ");
+using First;
+Console.WriteLine("Please enter first number");
+
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter second number");
+int b = int.Parse(Console.ReadLine());		
+
+calculator c = new calculator();
+c.Add(a,b);
+
 Console.ReadLine();
